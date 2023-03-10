eff77ba057a6ff885b594eef0ef57d2f62d64440
levBagryansky <28lev11@gmail.com>
2023-03-10 22:29:33 +0300

    Hello world

---
+++ intro/mpi_hello_world.c
@@ -0,0 +1,22 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Print off a hello world message
+    printf("Hello world, rank %d out of %d processors\n",
+        world_rank, world_size);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
