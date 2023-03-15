63e5620c2d2a89d32c562773d27f8146a5d1844a
levBagryansky <28lev11@gmail.com>
2023-03-15 12:08:22 +0300

    Now it creates a new communicator

--- Exclude_comm/exclude_communicator.c
+++ Exclude_comm/exclude_communicator.c
@@ -19,9 +19,20 @@ int main(int argc, char** argv) {
     int world_size = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    MPI_Group group_world;
+    MPI_Group group_world = 0;
     MPI_Comm_group(MPI_COMM_WORLD, &group_world);
+    printf("group_world %i\n", group_world);
 
-    MPI_Finalize();
+    MPI_Group excluded = 0;
+    int ranks[1] = {1};
+    MPI_Group_excl(group_world, 1, ranks, &excluded);
+    printf("excluded = %i\n", excluded);
+
+    //Create a new communicator from group
+    MPI_Comm new_comm;
+    MPI_Comm_create(MPI_COMM_WORLD, excluded, &new_comm);
+    printf("new_comm = %i\n", new_comm);
+
+
     return 0;
 }
