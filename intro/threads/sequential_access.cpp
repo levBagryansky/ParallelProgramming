a3acd14671a4d2f912195b735d4afa13c840273c
levBagryansky <28lev11@gmail.com>
2023-05-10 01:59:18 +0300

    order

--- intro/threads/sequential_access.cpp
+++ intro/threads/sequential_access.cpp
@@ -5,21 +5,25 @@
 
 std::mutex mutex;
 int cell = 0;
-
+int order = 0;
 int main(int argc, char **argv) {
     int threads = 10;
     if (argc == 2) {
         threads = static_cast<int> (strtol(argv[1], NULL, 10));
     }
-    auto f = []()
+    auto f = [](int i)
     {
+        while (order != i) {
+            sleep(0.0001);
+        }
         mutex.lock();
+        order += 1;
         cell += 1;
         mutex.unlock();
     };
     std::thread all[threads];
     for (int i = 0; i < threads; ++i) {
-        all[i] = std::thread(f);
+        all[i] = std::thread(f, i);
     }
     for (int i = 0; i < threads; ++i) {
         all[i].join();
