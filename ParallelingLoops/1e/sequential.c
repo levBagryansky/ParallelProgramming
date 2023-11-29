155ba976ffffeab0132291c2f1e238ed7125f15c
levBagryansky <28lev11@gmail.com>
2023-11-29 13:08:40 +0300

    YSIZE, XSIZE

--- ParallelingLoops/1e/sequential.c
+++ ParallelingLoops/1e/sequential.c
@@ -2,37 +2,37 @@
 #include <bits/types/FILE.h>
 #include <stdio.h>
 
-#define ISIZE 10
-#define JSIZE 10
+#define YSIZE 10
+#define XSIZE 10
 
 int main(int argc, char **argv) {
     char *out = "result.csv";
     if (argc == 2) {
         out = argv[1];
     }
-    double a[ISIZE][JSIZE];
-    int i, j;
+    double a[YSIZE][XSIZE];
+    int y, x;
     FILE *ff;
-    for (i=0; i<ISIZE; i++){
-        for (j=0; j<JSIZE; j++){
-            a[i][j] = 10*i +j;
+    for (y=0; y < YSIZE; y++){
+        for (x=0; x < XSIZE; x++){
+            a[y][x] = 10 * y + x;
         }
     }
 
 //начало измерения времени
-    for (i=1; i<ISIZE; i++){
-        for (j = 8; j < JSIZE; j++){
-            a[i][j] = sin(5*a[i-1][j-8]);
+    for (y=1; y < YSIZE; y++){
+        for (x = 8; x < XSIZE; x++){
+            a[y][x] = sin(5 * a[y - 1][x - 8]);
         }
     }
 //окончание измерения времени
 
     ff = fopen(out,"w");
-    for(i= 0; i < ISIZE; i++){
-        for (j= 0; j < JSIZE; j++){
-            fprintf(ff,"%f ",a[i][j]);
-            printf("%f ",a[i][j]);
-            if (j < JSIZE - 1) {
+    for(y= 0; y < YSIZE; y++){
+        for (x= 0; x < XSIZE; x++){
+            fprintf(ff,"%f ",a[y][x]);
+            printf("%f ",a[y][x]);
+            if (x < XSIZE - 1) {
                 fprintf(ff,", ");
                 printf(", ");
             }
